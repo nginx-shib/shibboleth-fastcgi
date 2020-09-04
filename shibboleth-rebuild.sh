308c17ae0cede2b6cd70daa3581e0c1685fec4c8
David Beitey <david@davidjb.com>
2020-09-04 14:32:46 +1000

    Fix build on CentOS 6

--- shibboleth-rebuild.sh
+++ shibboleth-rebuild.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 
+# Handle different package manager changes
 if [ -x "$(command -v dnf)" ]; then
     pkgmanager="dnf"
     download_cmd="dnf download"
@@ -13,6 +14,14 @@ else
     download_cmd="yumdownloader"
     builddep_cmd="yum-builddep"
 fi
+
+# EL6 requires specific packages
+os_version=$(rpm -qa --queryformat '%{VERSION}' '(redhat|sl|slf|centos|oraclelinux)-release(|-server|-workstation|-client|-computenode)')
+case $os_version in
+    6*)
+      builddep_pkgs="httpd-devel"
+    ;;
+esac
 
 # Download specific Shibboleth version or just the latest version
 if [ "$_SHIBBOLETH_VERSION" ]; then
@@ -28,7 +37,9 @@ sudo $builddep_cmd -y shibboleth*.src.rpm
 # with dnf builddep (or yum-builddep) so the optional PreReq packages need to
 # be manually installed Info is scarce on this topic but see
 # https://github.com/ceph/ceph/pull/8016
-sudo $pkgmanager install -y fcgi-devel
+sudo $pkgmanager install -y \
+    fcgi-devel \
+    $builddep_pkgs
 
 # Rebuild with FastCGI support
 rpmbuild --rebuild shibboleth*.src.rpm --with fastcgi
